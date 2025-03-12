9730d13b8874e2b99ad7ef917779db4b6b575c68
Lv Peng <lvpeng990324@qq.com>
2025-03-12 18:36:07 +0800

    首页改动

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -1,12 +1,50 @@
 <script lang="ts" setup>
-import { useUserStore } from "@/pinia/stores/user"
-import Admin from "./components/Admin.vue"
-import Editor from "./components/Editor.vue"
+import { ref } from "vue"
 
-const userStore = useUserStore()
-const isAdmin = userStore.roles.includes("admin")
+const order_count = ref(0)
+const supplier_count = ref(0)
+
+order_count.value = 10
 </script>
 
 <template>
-  <component :is="isAdmin ? Admin : Editor" />
+  <div class="app-container">
+    <el-row :gutter="16">
+      <el-col :span="8">
+        <div class="statistic-card">
+          <el-statistic :value="order_count">
+            <template #title>
+              <div style="display: inline-flex; align-items: center">
+                订单数
+              </div>
+            </template>
+          </el-statistic>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="statistic-card">
+          <el-statistic :value="supplier_count">
+            <template #title>
+              <div style="display: inline-flex; align-items: center">
+                供应商数
+              </div>
+            </template>
+          </el-statistic>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
+
+<style scoped>
+.el-statistic {
+  --el-statistic-content-font-size: 28px;
+}
+
+.statistic-card {
+  height: 100%;
+  padding: 20px;
+  border-radius: 4px;
+  background-color: var(--el-bg-color-overlay);
+}
+</style>
