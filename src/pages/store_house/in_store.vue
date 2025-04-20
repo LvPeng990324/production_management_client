8830eb3e3ad1ad3a8d086afad5ee52301548dd1e
Lv Peng <lvpeng990324@qq.com>
2025-04-20 20:57:53 +0800

    入库页面基础

--- src/pages/store_house/in_store.vue
+++ src/pages/store_house/in_store.vue
@@ -1,9 +1,49 @@
 <script lang="ts" setup>
+import { ref } from "vue"
 
+const value1 = ref([])
+const options = [
+  {
+    value: "Option1",
+    label: "Option1"
+  },
+  {
+    value: "Option2",
+    label: "Option2"
+  },
+  {
+    value: "Option3",
+    label: "Option3"
+  },
+  {
+    value: "Option4",
+    label: "Option4"
+  },
+  {
+    value: "Option5",
+    label: "Option5"
+  }
+]
 </script>
 
 <template>
   <div class="app-container">
-    in_store
+    <el-card shadow="never">
+      <div class="m-4">
+        <p>default</p>
+        <el-select
+          v-model="value1"
+          placeholder="Select"
+          style="width: 240px"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </div>
+    </el-card>
   </div>
 </template>
